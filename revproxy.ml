5628570f9a80156848d9dbc216fe4acfcaab7207
Péter Puha <peterpuha@Peters-MacBook-Air-2.local>
2026-02-16 19:55:09 +0100

    force upstream prefix

--- revproxy.ml
+++ revproxy.ml
@@ -16,15 +16,16 @@ module Make
     let path = Request.uri req |> Uri.path in
     match Upstream_store.get_upstream path with
       | None -> H.respond_error ~status:(`Bad_gateway) ~body:String.empty ()
-      | Some path ->
-        let upstream_uri = Uri.of_string path in
+      | Some { prefix; upstream_uri } ->
+        let prefix = if String.ends_with ~suffix:"/" prefix then prefix else prefix ^ "/" in
         let target =
           Uri.with_uri
             ~scheme:(Uri.scheme upstream_uri)
             ~host:(Uri.host upstream_uri)
             ~port:(Uri.port upstream_uri)
-            ~path:(Some (Uri.path upstream_uri))
+            ~userinfo: (Uri.userinfo upstream_uri)
             (Request.uri req)
+          |> Upstream.strip_path_prefix ~prefix
           in
           Log.info (fun print -> print "Calling %s" (target |> Uri.to_string));
           C.call ~ctx ~headers ~body (Request.meth req) target >>= fun (resp, body) ->
--- upstream.ml
+++ upstream.ml
@@ -1,9 +1,28 @@
+type t = {
+  prefix : string;
+  upstream_uri  : Uri.t;
+}
+
 module type STORE = sig
-  val get_upstream : string -> string option
+  val get_upstream : string -> t option
 end
 
 module Static_store = struct
   let get_upstream path =
-    if path = "/example" then (Some "http://localhost:7451")
+    if String.starts_with ~prefix: "/example" path
+      then (Some {
+        prefix = "/example";
+        upstream_uri = Uri.of_string "http://localhost:7451"
+      })
     else None
 end
+
+let strip_path_prefix uri ~prefix =
+  let path = Uri.path uri in
+  let path' =
+    if String.starts_with ~prefix path then
+      String.sub path
+        (String.length prefix)
+        (String.length path - String.length prefix)
+      else path in
+  Uri.with_path uri path'
